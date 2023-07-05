7f6b4b2e046656539f8728df1aa0255432b1bd1b
Prince <princeraj.chauhan@outlook.com>
2023-07-05 23:26:55 +0530

    Digital made also

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 
+// let tick = document.getElementById('tick').click()
 
 
-
-function rotate(){
+function rotate() {
     let d = new Date()
     let hour = d.getHours()
     let min = d.getMinutes()
     let sec = d.getSeconds()
 
-    document.getElementById('hour').style.transform = `rotate(${(hour*30)+(min*0.5)}deg)`
-    document.getElementById('min').style.transform = `rotate(${min*6}deg)`
-    document.getElementById('sec').style.transform = `rotate(${sec*6}deg)`
+    document.getElementById('hour').style.transform = `rotate(${(hour * 30) + (min * 0.5)}deg)`
+    document.getElementById('min').style.transform = `rotate(${min * 6}deg)`
+    document.getElementById('sec').style.transform = `rotate(${sec * 6}deg)`
+
+    new Audio('./ticking.mp3').play()
 }
 
 setInterval(() => {
     rotate()
+    digitalClock()
 }, 1000);
+
+
+function digitalClock(){
+    let d = new Date()
+    let h = d.getHours()
+    h = h>12 ? h%12 : h
+    let m = d.getMinutes()
+    m = m<10 ? '0'+m : m
+    let s = d.getSeconds()
+    s = s<10 ? '0'+s : s
+
+    document.getElementById('hr').innerHTML = h
+    document.getElementById('minute').innerHTML = m
+    document.getElementById('second').innerHTML = s
+}
+
